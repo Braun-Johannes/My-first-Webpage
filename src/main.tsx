1f4da16a397a2dab49837205cd67a49edad0708e
Braun-Johannes <Braun-Johannes@outlook.de>
2024-09-12 16:52:39 +0200

    changed meta content

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import "./index.css";
-
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Datenschutz from "./components/Datenschutz";
 import Impressum from "./components/Impressum";
-import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
