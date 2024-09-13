003c4fa154a9e2982d5fce008466441a992f473a
Braun-Johannes <Braun-Johannes@outlook.de>
2024-09-13 19:33:20 +0200

    changed summary

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -17,11 +17,24 @@ export default function Summary({
       </div>
       <div className="cardTextContainer">
         <p className="cardText">
-          Motivierter Webentwickler mit Ausbildung und Erfahrung in der
-          Erstellung dynamischer Webanwendungen mit JavaScript, HTML, CSS und
-          REACT. Umfangreicher Hintergrund in der Front-End- und
-          Back-End-Entwicklung mit einer Leidenschaft für kontinuierliches
-          Lernen und Verbesserung.
+          Als Full-Stack Webentwickler konzipiere und entwickle ich vollständig
+          skalierbare Webanwendungen, die sowohl im Frontend als auch im Backend
+          nahtlos ineinandergreifen. Im Frontend liegt mein Fokus auf der
+          Erstellung nutzerzentrierter, responsiver Interfaces, die auf
+          Performance und Benutzerfreundlichkeit optimiert sind.
+          <br />
+          <br />
+          Im Backend entwickle ich robuste, sichere und flexible Architekturen,
+          die reibungslose Datenverarbeitung und -speicherung ermöglichen. Durch
+          mein tiefes Verständnis von API-Entwicklung und Datenbankdesign kann
+          ich Projekte effizient umsetzen und maßgeschneiderte Lösungen für
+          spezifische Anforderungen bieten.
+          <br />
+          <br />
+          Mit meiner Erfahrung bin ich für Unternehmen interessant, die auf der
+          Suche nach einem Entwickler sind, der nicht nur Code schreibt, sondern
+          komplette Anwendungen erstellt und dabei sowohl die Benutzererfahrung
+          als auch die Systemperformance maximiert.
         </p>
       </div>
     </div>
